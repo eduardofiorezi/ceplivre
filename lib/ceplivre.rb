43804c29002c498bde36a05a84df7657b0bdd859
Felipe Bazzarella <fbazzarella@gmail.com>
2012-02-07 11:12:33 -0200

    add search by logradouro

--- lib/ceplivre.rb
+++ lib/ceplivre.rb
@@ -2,6 +2,7 @@ require "ceplivre/version"
 require "ceplivre/configuration"
 
 require "httparty"
+require "uri"
 
 module CepLivre
   class Cep
@@ -9,6 +10,15 @@ module CepLivre
     
     def self.find(cep, format)
       self.get("http://ceplivre.com.br/consultar/cep/#{CepLivre::Configuration.key}/#{cep}/#{format}")["cep"]
+    end
+  end
+
+  class Logradouro
+    include HTTParty
+    
+    def self.find(logradouro, format)
+    	logradouro_escaped = URI.escape(logradouro, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      self.get("http://ceplivre.com.br/consultar/logradouro/#{CepLivre::Configuration.key}/#{logradouro_escaped}/#{format}")["cep"]
     end
   end
 end
